493c3949564c5d5624095e13adcf713cc1cbfeb5
Muji-47it <107984150+Muji-47it@users.noreply.github.com>
2022-06-24 15:44:51 +0500

    Update arrayassignment.cpp

--- arrayassignment.cpp
+++ arrayassignment.cpp
@@ -1,9 +1,18 @@
-#include<iostream>
-using namesapce std;
-int main()
-{
-    int arr[]={3,4,5};
-    int arr2[]=arr[];
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int arr[3]={3,4,5};
+    int arr2[3];
+  /*  arr2[0]=arr[0];
+    arr2[1]=arr[1];
+    arr2[2]=arr[2];
+    cout<<arr2[0];
+    cout<<arr2[1];
+    cout<<arr2[2];*/
+    
+   for(int i=0;i<3;i++)
+    arr2[i]=arr[i];
     for(int i=0;i<3;i++)
-    cout<<arr2[i];
+    cout<<arr2[i]<<endl;
 }
